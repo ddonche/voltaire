881316c27138446421c182e5c7a188efb0709302
ddonche <dan@fatalfitness.com>
2017-05-24 05:29:04 +0000

    updated module

--- lib/voltaire.rb
+++ lib/voltaire.rb
@@ -1,17 +1,17 @@
 require "voltaire/version"
 
 module Voltaire
+  class Reputation  
+    def voltaire_up(amount, reputation, user)
+      amount.times.collect do
+        User.increment_counter(reputation, user)
+      end
+    end
     
-  def voltaire_up(amount, reputation, user)
-    amount.times.collect do
-      User.increment_counter(reputation, user)
+    def voltaire_down(amount, reputation, user)
+      amount.times.collect do
+        User.decrement_counter(reputation, user)
+      end
     end
   end
-  
-  def voltaire_down(amount, reputation, user)
-    amount.times.collect do
-      User.decrement_counter(reputation, user)
-    end
-  end
-  
 end
