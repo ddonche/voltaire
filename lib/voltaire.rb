0c71b6aceca73838ce3864832ca4e29da8962469
ddonche <dan@fatalfitness.com>
2017-05-24 05:52:58 +0000

    removed self in methods

--- lib/voltaire.rb
+++ lib/voltaire.rb
@@ -1,13 +1,13 @@
 require "voltaire/version"
 
 module Voltaire
-  def self.voltaire_up(amount, reputation, user)
+  def voltaire_up(amount, reputation, user)
     amount.times.collect do
       User.increment_counter(reputation, user)
     end
   end
   
-  def self.voltaire_down(amount, reputation, user)
+  def voltaire_down(amount, reputation, user)
     amount.times.collect do
       User.decrement_counter(reputation, user)
     end
