9fc2f57281b9ee3350ef83fabd56ab57e262285f
ddonche <dan@fatalfitness.com>
2017-05-24 06:03:34 +0000

    changed module methods

--- lib/voltaire.rb
+++ lib/voltaire.rb
@@ -1,15 +1,17 @@
 require "voltaire/version"
 
 module Voltaire
-  def voltaire_up(amount, reputation, user)
-    amount.times.collect do
-      User.increment_counter(reputation, user)
+  class Reputation
+    def voltaire_up(amount, reputation, user)
+      amount.times.collect do
+        User.increment_counter(reputation, user)
+      end
     end
-  end
-  
-  def voltaire_down(amount, reputation, user)
-    amount.times.collect do
-      User.decrement_counter(reputation, user)
+    
+    def voltaire_down(amount, reputation, user)
+      amount.times.collect do
+        User.decrement_counter(reputation, user)
+      end
     end
   end
 end
